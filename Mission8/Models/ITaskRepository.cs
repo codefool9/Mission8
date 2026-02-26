0c0e41ac5c5b9de91779ee63b239fdeee71f395a
mitchsmii <143842296+mitchsmii@users.noreply.github.com>
2026-02-26 13:18:35 -0700

    Changed the names of the methods within repository to match views

--- Mission8/Models/ITaskRepository.cs
+++ Mission8/Models/ITaskRepository.cs
@@ -5,6 +5,6 @@ public interface ITaskRepository
     List<TaskItem> TaskItems { get; }
     List<Category> Categories { get; }
     public void AddTask(TaskItem task);
-    public void UpdateTask(TaskItem task);
+    public void EditTask(TaskItem task);
     public void DeleteTask(int id);
 }
